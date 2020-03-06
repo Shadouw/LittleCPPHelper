a2d8c6dae803aeec380d21ac903018d96bc4a1c1
Rolf Schatten <Rolf.Schatten@gmail.com>
2020-03-06 18:46:27 +0100

    Renamed strings.h into string.hpp

--- test/LittleCPPHelperTest.cpp
+++ test/LittleCPPHelperTest.cpp
@@ -1,7 +1,7 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include <catch2/catch.hpp>
 
-#include <LittleCPPHelper/strings.h>
+#include <LittleCPPHelper/string.hpp>
 
 
 TEST_CASE("Trim some strings", "[LittleCPPHelper::strings]") {
